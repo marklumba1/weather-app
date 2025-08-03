d688c9519dd6ca648ef5fbc863d6621e5c4ef18a
Mark Lumba <Mark.Lumba.-ND@disney.com>
2025-08-03 07:58:44 -0400

    fix-card-caomponent

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,10 +6,10 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, data, icon }) => {
   return (
     <div className=" p-5 flex justify-between sm:flex-col gap-3">
-      <p className="flex items-center gap-3">
+      <div className="flex sm:flex-row min-w-[80px] -mx-4 sm:mx-0  flex-col items-center sm:gap-3">
         <i className={`wi wi-${icon} text-3xl w-10 sm:w-auto text-center `} />
-        {title}
-      </p>
+        <p className="text-xs sm:text-base text-center">{title}</p>
+      </div>
       <p className=" text-xl sm:text-2xl font-bold">{data}</p>
     </div>
   );
